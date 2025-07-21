65fad7ade2a63bc123f39f2a65a4ac796c528d86
Jorge González <jgp19112003@gmail.com>
2025-07-21 20:10:17 +0200

    creacion proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <main className="flex-center h-[100vh]">
+      <h1 className="text-3xl font-bold">Day by Day</h1>
+    </main>
+  );
+};
+
+export default App;
