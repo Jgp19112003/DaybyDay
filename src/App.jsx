e6c3da7986b512c88c6a7383e331ad28cbf210eb
Jorge González <jgp19112003@gmail.com>
2025-07-22 12:13:20 +0200

    servicios dev

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import NavBar from "./components/NavBar";
+import WhatsAppButton from "./components/WhatsAppButton";
+import Servicios from "./components/Servicios";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -9,6 +11,8 @@ const App = () => {
   return (
     <main>
       <NavBar />
+      <WhatsAppButton />
+      <Servicios />
     </main>
   );
 };
